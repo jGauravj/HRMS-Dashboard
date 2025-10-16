7f76fd28ea07a9fceaf736e83cce701450591f37
Gaurav <gouraw1@gmail.com>
2025-10-17 01:07:42 +0530

    add some style on login & signup card

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { Link } from "react-router-dom";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
 const Login = () => {
   const { login } = useAuth();
@@ -13,34 +16,39 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="flex items-center justify-center h-screen bg-background">
       <form
         onSubmit={handleSubmit}
-        className="bg-white p-6 rounded-lg shadow-md w-96"
+        className="bg-card p-6 rounded-lg shadow-sm border w-sm"
       >
         <h2 className="text-2xl font-semibold mb-4 text-center">Login</h2>
-        <input
-          type="email"
-          placeholder="Email"
-          className="border p-2 w-full mb-3 rounded"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className="border p-2 w-full mb-3 rounded"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
-        >
-          Login
-        </button>
+        <div className="flex flex-col gap-1">
+          <div className="flex flex-col gap-1.5">
+            <Label>Email</Label>
+            <Input
+              type="email"
+              placeholder="Email"
+              className="mb-2"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="flex flex-col gap-1.5">
+            <Label>Password</Label>
+            <Input
+              type="password"
+              placeholder="Password"
+              className=""
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <Button type="submit" className="w-full mt-3">
+            Login
+          </Button>
+        </div>
         <p className="mt-3 text-center text-sm">
           Don’t have an account?{" "}
           <Link to="/signup" className="text-blue-600 font-semibold">
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { Link } from "react-router-dom";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
 const Signup = () => {
   const { signup } = useAuth();
@@ -14,42 +17,49 @@ const Signup = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="flex items-center justify-center h-screen bg-background">
       <form
         onSubmit={handleSubmit}
-        className="bg-white p-6 rounded-lg shadow-md w-96"
+        className="bg-card p-6 rounded-lg shadow-sm w-sm"
       >
         <h2 className="text-2xl font-semibold mb-4 text-center">Signup</h2>
-        <input
-          type="text"
-          placeholder="Full Name"
-          className="border p-2 w-full mb-3 rounded"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          className="border p-2 w-full mb-3 rounded"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className="border p-2 w-full mb-3 rounded"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button
-          type="submit"
-          className="w-full bg-green-500 text-white py-2 rounded hover:bg-green-600"
-        >
-          Signup
-        </button>
+        <div className="flex flex-col gap-1">
+          <div className="flex flex-col gap-1.5">
+            <Label>Full Name</Label>
+            <Input
+              type="text"
+              placeholder="Full Name"
+              value={name}
+              className="mb-2"
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="flex flex-col gap-1.5">
+            <Label>Email</Label>
+            <Input
+              type="email"
+              placeholder="Email"
+              className="mb-2"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="flex flex-col gap-1.5">
+            <Label>Password</Label>
+            <Input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <Button type="submit" className="w-full mt-3">
+            Signup
+          </Button>
+        </div>
         <p className="mt-3 text-center text-sm">
           Already have an account?{" "}
           <Link to="/login" className="text-blue-600 font-semibold">
